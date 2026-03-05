1310327ccfc62854ca7a8dcc52736d00e7e64049
Aleksandar Radovanović <radovanovicsasha.rs@gmail.com>
2026-03-05 10:34:18 +0100

    Reveret BaseTest to original, formed utils/DataFactory

---
+++ src/test/java/utils/DriverFactory.java
@@ -0,0 +1,23 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+
+    public static WebDriver createChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
+
+        boolean isCI = "true".equalsIgnoreCase(System.getenv("CI"));
+
+        if (isCI) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        return new ChromeDriver(options);
+    }
+}
